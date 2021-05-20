57a8ad0da8e07e25c5fcf4061ebd8a614c59abab
luissalazar <l_salazar85@icloud.com>
2021-05-19 22:23:03 -0300

    Flexbox media querie

---
+++ flexbox/main.js
@@ -0,0 +1,9 @@
+function comparar(num1, num2){
+    if(num1 < num2){
+        console.log(`Incrementar en 1 unidad el valor de ${num1} no lo hace mayor o igual que ${num2}`)
+    } else {
+        console.log(`${num2} es mayor que ${num1}`)
+    }
+}
+
+comparar(0,1)
