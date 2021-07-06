68decf8e8be2b2b79ae7e9558c2d13861f962e79
luissalazar <l_salazar85@icloud.com>
2021-07-05 21:52:18 -0300

    Variables, Tipo de datos, operadores

---
+++ javascript/variables-datos/main.js
@@ -0,0 +1,39 @@
+// Variables ( var, let, const )
+
+// Forma basica de declarar una variable
+var primerNombre = "Luis"
+
+// Usamos const cuando el valor de la variable no será modificado
+const segundoNombre = "Alfredo"
+
+// Usamos let cuando el valor de la varible puede mutar sin generar interpretaciones no deseadas
+let primerApellido = "Salazar"
+
+// Tipo de datos
+// ( Number - String - Boolean - Undefined)
+
+// Number
+var age = 35;
+
+// String
+var word = "Luis"
+
+// Boolean
+var verdad = true
+var mentira = false
+
+// Undefined ( No tiene valor de referencia )
+var nucba
+
+
+// Operadores
+// Aritmeticos ( sumar, restar, dividir, multiplicar)
+var num1 = 10
+var num2 = 20
+
+// TypeOf
+console.log(typeof num1)
+
+// Identidad o igualdad ( === , !== , == )
+console.log(num1 !== num2)
+
