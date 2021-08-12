00fb4695e1a07d31e722e3d661c8a5bef3e6f2a3
luissalazar <l_salazar85@icloud.com>
2021-08-12 00:16:01 -0300

    Form validation

---
+++ javascript/class-form/main.js
@@ -0,0 +1,32 @@
+function updateBg(reset){
+    const cuadrado = document.getElementById('cuadrado')
+    reset ? cuadrado.classList.remove('extra') :
+    cuadrado.classList.add('extra')
+}
+
+
+// Form
+const myForm = event => {
+    event.preventDefault()
+    const mailformat =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    const formState = []
+    let name = document.getElementById('nombre')
+    let lastname = document.getElementById('apellido')
+    let email = document.getElementById('email')
+
+    !name.value ? name.classList.add('error'):
+    formState.push({ element: 'nombre', value: name.value }) && name.classList.remove('error')
+
+    !lastname.value ? lastname.classList.add('error') :
+    formState.push({ element: 'apellido', value: lastname.value }) && lastname.classList.remove('error')
+
+    !email.value || !email.value.match(mailformat) ? email.classList.add('error') :
+    formState.push({ element: 'email', value: email.value }) && email.classList.remove('error')
+
+    if(formState.length === 3){
+        const dato = document.getElementById('formValues')
+        dato.innerHTML = JSON.stringify(formState)
+    }
+}
+
+
