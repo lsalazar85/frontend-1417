cece74a6a503fb70ac059af9ea72246d3a85350e
luissalazar <l_salazar85@icloud.com>
2021-08-18 21:32:19 -0300

    Validacion de formularios Pt 2

--- javascript/class-form/main.js
+++ javascript/class-form/main.js
@@ -8,13 +8,13 @@ function updateBg(reset){
 // Form
 const myForm = event => {
     event.preventDefault()
-    const mailformat =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
     const formState = []
+    const mailformat =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
     let name = document.getElementById('nombre')
     let lastname = document.getElementById('apellido')
     let email = document.getElementById('email')
 
-    !name.value ? name.classList.add('error'):
+    !name.value ? name.classList.add('error') :
     formState.push({ element: 'nombre', value: name.value }) && name.classList.remove('error')
 
     !lastname.value ? lastname.classList.add('error') :
@@ -23,10 +23,15 @@ const myForm = event => {
     !email.value || !email.value.match(mailformat) ? email.classList.add('error') :
     formState.push({ element: 'email', value: email.value }) && email.classList.remove('error')
 
-    if(formState.length === 3){
-        const dato = document.getElementById('formValues')
-        dato.innerHTML = JSON.stringify(formState)
-    }
+    const dato = document.getElementById('formValues')
+
+    const html = formState.map(item => item.value).forEach(item => {
+        let p = document.createElement('p')
+        p.innerText = item
+        return dato.appendChild(p)
+    })
+
+    formState.length === 3 && JSON.stringify(html)
 }
 
 
