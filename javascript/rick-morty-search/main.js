72a4067d47c9017aa0250982a5308ed80e467294
luissalazar <l_salazar85@icloud.com>
2021-10-06 20:35:26 -0300

    Delte comments

--- javascript/rick-morty-search/main.js
+++ javascript/rick-morty-search/main.js
@@ -29,31 +29,3 @@ form.addEventListener('submit', async (event) => {
         character.appendChild(ul)
     })
 })
-
-/*
-window.addEventListener('load', () => {
-    const character = document.getElementById('characters')
-    const ul = document.createElement('ul')
-    const data = JSON.parse(localStorage.getItem('characters'))
- /*
-   const getData = () => {
-       return fetch(URL)
-       .then(response => response.json())
-       .then(characters => localStorage.setItem('characters', JSON.stringify(characters.results.flatMap(item => ({ name: item.name, image: item.image })))))
-   }
-
-   getData()
-
-   data && data.map(item => {
-        const li = document.createElement('li')
-        const img = document.createElement('img')
-        const span = document.createElement('span')
-        span.appendChild(document.createTextNode(item.name))
-        img.src = item.image
-        li.appendChild(img)
-        li.appendChild(span)
-        ul.appendChild(li)
-        character.appendChild(ul)
-    }) 
-
-}) */
